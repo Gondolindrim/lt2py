50ae8b5df26b329410c6d14e2b1c0078641a963d
Gondolindrim <alvaro.volpato@usp.br>
2020-08-06 03:58:51 -0300

    Better code for time series data

--- lt2py.py
+++ lt2py.py
@@ -5,98 +5,50 @@ import matplotlib.pyplot as pyplot
 import matplotlib as mplot
 from matplotlib import rc
 
-with open('cmrr.txt', 'r', encoding='ISO-8859-1') as f:
+import tkinter as tk
+from tkinter import filedialog
 
-	# Counting number of rows in file
-	for k, i in enumerate(f):
-		pass
+root = tk.Tk()
+root.withdraw()
 
-	# Number of rows in the file, which is given by number of data points
-	lineLen = k
-	f.seek(0)
+# File prompt to ask for the file path
+file_path = filedialog.askopenfilename()
 
-	pointer1 = 4
-	line = f.readline()
+with open(file_path, 'r', encoding='ISO-8859-1') as f:
 
-	# Counter for the number of traces in the file
-	dataTraces = 0
-	i = 0
-	labels = [];
-	while 1:
+	line = f.readline().split('\t')
 
-		pointer1 += 1
+	# For some reason the last element of 'line' comes with a '\n' last element. Removing this '\n' character:
+	line[-1] = line[-1][:-1]
 
-		while line[pointer1]!='\t':
-			pointer1 += 1
-
-		pointer2 = pointer1 + 1
-
-		while line[pointer2]!='\t' and line[pointer2]!='\n':
-			pointer2 += 1
-
-		labels.append(str(line[pointer1+1:pointer2]))
-
-		dataTraces += 1
-		i += 1
-
-		if line[pointer2] == '\n':
-			break
-
+	# This first line contains the names of the signals. signal_names stores their values.
+	signal_names = line
 	
-	# Initializing data
-	gain = 0*np.ones((lineLen,dataTraces))
-	phase = 0*np.ones((lineLen,dataTraces))
-	freq = 0*np.ones((lineLen,1))
+	# series_data storess the raw data, including time values
+	series_data = []
+	for line in f:
+		line_temp = [float(x) for x in line.split('\t')]
+		series_data.append(line_temp)
 
-	for i in range(lineLen):
-		
-		line = f.readline()
-		freq[i,0] = float(line[0:22])
+	series_data = np.array(series_data)	
 
-		#print('\n{0:8.5f}'.format(freq[i,0]))
+# point_number stores the number of time series datapoints
+# curve_number stores the number of time data curves in the data set
+point_number = len(series_data)
+curve_number = len(series_data[0]) - 1
 
-		pointer = 22
-		pointer2 = 0
-		
-		for k in range(dataTraces):
-			
-			while line[pointer2]!=',':
-				pointer2 += 1
-
-			gain[i,k] = float(line[pointer+1:pointer2-2])
-			#print(gain[i,k])
-
-			pointer = pointer2
-
-			while line[pointer2]!=')':
-				pointer2 += 1
-
-			phase[i,k] = float(line[pointer+1:pointer2-1])
-			#print(phase[i,k])
-
-			if k < dataTraces - 1:
-				while line[pointer]!='(':
-					pointer += 1
+time_values = [x[0] for x in series_data]
 
 fig1 = pyplot.figure();
 ax1 = fig1.add_axes([0.1,0.1,0.8,0.8]);
 
-fig2 = pyplot.figure();
-ax2 = fig2.add_axes([0.1,0.1,0.8,0.8]);
+for k in range(curve_number):
+	ax1.plot(time_values, [x[k+1] for x in series_data],label=signal_names[k+1],linewidth=1)
 
-for k in range(dataTraces):
-	ax1.semilogx(freq[:,0],gain[:,k],label=labels[k],linewidth=1)
-	ax2.semilogx(freq[:,0],phase[:,k],label=labels[k],linewidth=1)
-
-ax1.legend(loc='bottom left')
-ax2.legend(loc='upper left')
+ax1.legend(loc='best')
 
 ax1.grid(which='major')
 ax1.grid(which='minor',dashes=(5,2))
-ax1.set_xlim([min(freq),max(freq)])
-
-ax2.grid(which='major')
-ax2.grid(which='minor',dashes=(5,2))
-ax2.set_xlim([min(freq),max(freq)])
+ax1.set_xlim([min(time_values),max(time_values)])
 
 pyplot.show()
