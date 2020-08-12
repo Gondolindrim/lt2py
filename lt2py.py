0a95ebd7cd26dcfb7aafe97ae67eb3dfc3f70d27
Gondolindrim <alvaro.volpato@usp.br>
2020-08-11 21:40:05 -0300

    Add quality of life improvements
    - Add GUI to select target file and select target curves
    - Add arguments option for the target file

--- lt2py.py
+++ lt2py.py
@@ -5,14 +5,27 @@ import matplotlib.pyplot as pyplot
 import matplotlib as mplot
 from matplotlib import rc
 
-import tkinter as tk
-from tkinter import filedialog
+import easygui as eg
 
-root = tk.Tk()
-root.withdraw()
+import matplotlib.cm as cm
 
-# File prompt to ask for the file path
-file_path = filedialog.askopenfilename()
+import argparse
+parser = argparse.ArgumentParser(
+	prog = 'LT2PY API',
+	description = 'A Python API for LTSpiceXVII plots.',
+	)
+
+parser.add_argument(	'--plotfile',
+			type = str,
+			metavar = 'PLOTFILE',
+			default = '',
+			help='Target LTSpice-exported *.txt file containing plot data')
+
+args = parser.parse_args()
+file_path = args.plotfile
+if file_path == '':
+	# File prompt to ask for the file path
+	file_path = eg.fileopenbox(msg='Select simulation data *.txt file', title='LT2PY file selection', default='', filetypes=["*.txt"], multiple=False)
 
 with open(file_path, 'r', encoding='ISO-8859-1') as f:
 
@@ -23,32 +36,144 @@ with open(file_path, 'r', encoding='ISO-8859-1') as f:
 
 	# This first line contains the names of the signals. signal_names stores their values.
 	signal_names = line
-	
-	# series_data storess the raw data, including time values
-	series_data = []
-	for line in f:
-		line_temp = [float(x) for x in line.split('\t')]
-		series_data.append(line_temp)
 
-	series_data = np.array(series_data)	
+	# If the second line contains the "Step information" string, this means that the file contains a stepped dataset.
+	line = f.readline()
+	if 'Step Information' in line:
+		stepped_dataset = True
+		f.seek(0)
+		f.readline()
+		# step_data storess the raw data, including time values, as a list of two-dimensional series_data. In the case of a stepped dataset, it is comprised of a three-dimensional array where the first dimension is the step run, the second dimension is the number of the datasample, and the third index is the step run.
+		temp_series_data = []
+		step_data = []
+		step_info = []
 
-# point_number stores the number of time series datapoints
-# curve_number stores the number of time data curves in the data set
-point_number = len(series_data)
-curve_number = len(series_data[0]) - 1
+		for line in f:
+			if 'Step Information' in line:
+				# if the line contains "Step Information" then a step timeseries has ended. The temp time series is stored and the temp_series_data variable is cleared
+				step_info.append(line.replace('Step Information: ', '').replace('\n',''))
+				if temp_series_data != []: step_data.append(temp_series_data)
+				temp_series_data = []
 
-time_values = [x[0] for x in series_data]
+			else:
+				line_temp = [float(x) for x in line.split('\t')]
+				temp_series_data.append(line_temp)
 
-fig1 = pyplot.figure();
-ax1 = fig1.add_axes([0.1,0.1,0.8,0.8]);
 
-for k in range(curve_number):
-	ax1.plot(time_values, [x[k+1] for x in series_data],label=signal_names[k+1],linewidth=1)
+	else: #If the second line does not contain "Step information" then the dataset is not stepped.
+		stepped_dataset = False
+		f.seek(0)
+		f.readline()
+		
+		# series_data storess the raw data, including time values. In the case of a non-stepped dataset, it is comprised of a two-dimensional array where the vertical dimension is the number of the datasample and the horizontal direction is the curves index.
+		series_data = []
+		for line in f:
+			line_temp = [float(x) for x in line.split('\t')]
+			series_data.append(line_temp)
 
-ax1.legend(loc='best')
+		series_data = np.array(series_data)
+if stepped_dataset:
+	print('STEPPED!')
+	step_choice = eg.multchoicebox('What steps do you want to plot?' , 'LT2PY step selecion', step_info)
+	if step_choice == None:
+		print('No plot steps selected!')
+		raise SystemExit(0)
 
-ax1.grid(which='major')
-ax1.grid(which='minor',dashes=(5,2))
-ax1.set_xlim([min(time_values),max(time_values)])
+	step_choice_index = []
+	for x in step_choice:
+		for k in range(len(step_info)):
+			if x == step_info[k]:
+				step_choice_index.append('{}'.format(k))
+
+	step_choice_index = [int(x) for x in step_choice_index]
+
+	curve_choice = eg.multchoicebox('What curves do you want to plot?' , 'LT2PY curve plot selecion', signal_names[1:])
+	if curve_choice == None:
+		print('No plot curves selected!')
+		raise SystemExit(0)
+
+	curve_choice_index = []
+	for x in curve_choice:
+		for k in range(len(signal_names)):
+			if x == signal_names[k]:
+				curve_choice_index.append('{}'.format(k))
+
+	curve_choice_index = [int(x) for x in curve_choice_index]
+
+	timestamps = []
+	for step in step_data:
+		timestamps.append(np.array([x[0] for x in step]))
+
+	if eg.ynbox('Do you want to plot steps in individual figures?','Please confirm'):     # show a Continue/Cancel dialog
+		print('YES!')
+		fig_list = []
+		ax_list = []
+		for choice in step_choice: 
+			temp_fig = pyplot.figure()
+			temp_ax = temp_fig.add_axes([0.1,0.1,0.8,0.8]);
+			fig_list.append(temp_fig)
+			ax_list.append(temp_ax)
+
+		for step_choice_number, ax_choice in zip(step_choice_index, ax_list):
+			for curve_choice_number in curve_choice_index:
+				ax_choice.plot(timestamps[step_choice_number], [x[curve_choice_number] for x in step_data[step_choice_number]],label=signal_names[curve_choice_number],linewidth=1)
+		
+		for step_choice_number, ax in zip(step_choice_index, ax_list):
+			ax.legend(loc='best')
+			ax.grid(which='major')
+			ax.grid(which='minor',dashes=(5,2))
+			ax.set_xlim([min([min(x) for x in timestamps]),max([max(x) for x in timestamps])])
+			ax.set_title(step_info[step_choice_number])
+
+	else:
+		fig1 = pyplot.figure()
+		ax1 = fig1.add_axes([0.1,0.1,0.8,0.8]);
+
+		# If the data set is stepped but the user has only
+		if len(curve_choice) == 1:
+			colors = [cm.rainbow(x) for x in np.linspace(0, 1, len(step_choice))]
+			for step_choice_number, color_index in zip(step_choice_index, colors):
+				ax1.plot(timestamps[step_choice_number], [x[1] for x in step_data[step_choice_number]],linewidth=1, label=step_info[step_choice_number])
+		else:
+			colors = [cm.rainbow(x) for x in np.linspace(0, 1, len(curve_choice))]
+			temp_signal_names = signal_names
+			for step_choice_number in step_choice_index:
+				for curve_choice_number, color_index in zip(curve_choice_index, range(len(curve_choice_index))):
+					ax1.plot(timestamps[step_choice_number], [x[curve_choice_number] for x in step_data[step_choice_number]],label=temp_signal_names[curve_choice_number],linewidth=1, color=colors[color_index])
+					temp_signal_names[curve_choice_number] = '_nolegend_'
+
+		ax1.legend(loc='best')
+		ax1.grid(which='major')
+		ax1.grid(which='minor',dashes=(5,2))
+		ax1.set_xlim([min([min(x) for x in timestamps]),max([max(x) for x in timestamps])])
+
+
+else:
+	choice = eg.multchoicebox('What curves do you want to plot?' , 'LT2PY plot selecion', signal_names[1:])
+	if choice == None:
+		print('No plot curves selected!')
+		raise SystemExit(0)
+
+	choice_index = []
+	for x in choice:
+		for k in range(len(signal_names)):
+			if x == signal_names[k]:
+				choice_index.append('{}'.format(k))
+
+	choice_index = [int(x) for x in choice_index]
+
+	time_values = [x[0] for x in series_data]
+
+	fig1 = pyplot.figure();
+	ax1 = fig1.add_axes([0.1,0.1,0.8,0.8]);
+
+	for choice_number in choice_index:
+		ax1.plot(time_values, [x[choice_number] for x in series_data],label=signal_names[choice_number],linewidth=1)
+
+	ax1.legend(loc='best')
+
+	ax1.grid(which='major')
+	ax1.grid(which='minor',dashes=(5,2))
+	ax1.set_xlim([min(time_values),max(time_values)])
 
 pyplot.show()
